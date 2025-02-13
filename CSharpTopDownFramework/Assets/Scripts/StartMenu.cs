352b78860893c2c5382ec7b7ce50921915708840
CapturedGrub974 <kierandr06@gmail.com>
2025-02-13 10:57:39 +0000

    wte4

--- CSharpTopDownFramework/Assets/Scripts/StartMenu.cs
+++ CSharpTopDownFramework/Assets/Scripts/StartMenu.cs
@@ -6,34 +6,35 @@ using UnityEngine.UI;
 
 public class StartMenu : MonoBehaviour
 {
-
-    public Canvas quitMenu;
-    public Button startText;
-    public Button exitText;
+    public Canvas m_startMenu;
+    public Canvas m_controlsMenu;
+    public Canvas m_quitMenu;
+    public Button m_startText;
+    public Button m_controlsText;
+    public Button m_exitText;
 
     // Use this for initialization
     void Start()
     {
-
-        quitMenu = quitMenu.GetComponent<Canvas>();
-        startText = startText.GetComponent<Button>();
-        exitText = exitText.GetComponent<Button>();
-        quitMenu.enabled = false;
-
+        m_quitMenu = m_quitMenu.GetComponent<Canvas>();
+        m_startText = m_startText.GetComponent<Button>();
+        m_controlsText = m_controlsText.GetComponent<Button>();
+        m_exitText = m_exitText.GetComponent<Button>();
+        m_startMenu.enabled = true;
+        m_controlsMenu.enabled = false;
+        m_quitMenu.enabled = false;
     }
 
     public void ExitPress()
     {
-        quitMenu.enabled = true;
-        startText.enabled = false;
-        exitText.enabled = false;
+        m_quitMenu.enabled = true;
+        m_startMenu.enabled = false;
     }
 
     public void Nopress()
     {
-        quitMenu.enabled = false;
-        startText.enabled = true;
-        exitText.enabled = true;
+        m_quitMenu.enabled = false;
+        m_startMenu.enabled = true;
     }
 
     public void StartLevel()
@@ -44,5 +45,17 @@ public class StartMenu : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void Controls()
+    {
+        m_controlsMenu.enabled = true;
+        m_startMenu.enabled = false;
+    }
+
+    public void Back()
+    {
+        m_controlsMenu.enabled = false;
+        m_startMenu.enabled = true;
     }
 }
